35b6be22548c4914fe876e9722d7077e44041935
Henrique A <henriqueambrosano@hotmail.com>
2021-04-26 19:47:53 -0300

    improved code

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let quantity = Math.floor(Math.random() * 10000);
+//set the variables that will compose the random message
 let currency = ['Dolars', 'Bitcoins', 'Euros', 'Reais', 'Ethers', 'Ripples', 'Cardanos', 'Pesos Argentinos'];
 let years = [1,2,3,4,5,6,7,8,9,10];
-
+//random select an array element and output it
+let i = (array) =>{
+    let index = Math.floor(Math.random()* array.length);
+    return array[index]
+};
+//get the data together and output the random message
 let fortuneTeller = () =>{
-    let i = Math.floor(Math.random()* currency.length)
-    let j = Math.floor(Math.random() * years.length)
-    console.log(`You will have: ${quantity} ${currency[i]} in the next ${years[j]} years`)
+    let quantity = Math.floor(Math.random() * 10000);
+    return `You will have: ${quantity} ${i(currency)} in the next ${i(years)} years`
 }
+
+
